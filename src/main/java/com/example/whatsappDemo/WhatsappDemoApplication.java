e3fe101b4e4f63d333394d67c10a2400931a5c06
janivsroht <janivvin@gmail.com>
2026-04-28 09:34:00 +0530

    Initial commit: WhatsApp chatbot simulation

---
+++ src/main/java/com/example/whatsappDemo/WhatsappDemoApplication.java
@@ -0,0 +1,91 @@
+package com.example.whatsappDemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class WhatsappDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappDemoApplication.class, args);
+	}
+
+}
+
+class IncoimgMessage {
+    private String sender;
+    private String text;
+
+    public IncoimgMessage() {}
+
+    public String getSender() {
+        return sender;
+    }
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
+
+class Response{
+    private String reply;
+
+    public Response(String reply) {
+        this.reply = reply;
+    }
+
+    public String getReply() {
+        return reply;
+    }
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+}
+
+@RestController
+@RequestMapping("/webhook")
+class WebhookController {
+    private static final Logger logger = LoggerFactory.getLogger(WebhookController.class);
+
+    @PostMapping
+    public ResponseEntity<Response> handleMessage(@RequestBody IncoimgMessage message) {
+        logger.info("IncoimgMessage received: Sender:[{}], Text:[{}]", message.getSender(), message.getText());
+
+        String reply;
+        String text;
+        if (message.getText() !=null){
+            text = message.getText().trim().toLowerCase();
+        } else{
+            text = "";
+        }
+
+        switch (text){
+            case "hi":
+                text ="Hello";
+                break;
+            case "bye":
+                text ="Goodbye";
+                break;
+            default:
+                text = "I only know how to reply to hi and bye. But you can still chat with me and i will give the same response";
+                break;
+        }
+        Response response = new Response(text);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/test/java/com/example/whatsappDemo/WhatsappDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsappDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
